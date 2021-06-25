b667e54d8832bc3dcde18ef36c1b0cb2f4a6fe27
Kaito-design <Kaito.m_317@outlook.jp>
2021-06-25 23:17:38 +0900

    googlemap error

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 var http = require("http").Server(app);
-
+app.set("view engine", "ejs");
 app.get("/", function (req, res) {
     res.render("./googlemap.ejs");
 })
