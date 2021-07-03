8c727e7a93007bb9476a74bd3f2307e3e0743ea0
kajikentaro <k.kentarou0412@gmail.com>
2021-07-03 07:38:13 +0000

    fix first regist behavior

--- routes/auth.js
+++ routes/auth.js
@@ -56,8 +56,9 @@ router.get('/callback', async (req, res) => {
         (error, results) => {
             if (!results.length) {
                 console.log("初登録");
-                connection.query("select id from nodes where type=start;",
+                connection.query('select node_id from nodes where type="start";',
                     (error, results) => {
+                        console.log(results);
                         const node_id = results[Math.floor(Math.random() * results.length)];//ここ不安...
                         var registData = [userId, seq2_data.login, -1, node_id, 0];//-1は仕様
                         connection.query("insert into users(user_id, name, commit_count, node_id, step) values(?,?,?,?,?); ", registData);
