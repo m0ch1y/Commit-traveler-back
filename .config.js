73ef5eba3447d560f80123cc03ba225b9de04034
saityyy <sec200091@gmail.com>
2021-07-04 20:58:43 +0000

    add .config.js removed  access token

---
+++ .config.js
@@ -0,0 +1,7 @@
+exports.CLIENT_ID = "";
+exports.CLIENT_SECRET = "";
+
+exports.USER_URI = 'https://api.github.com/user';
+exports.REDIRECT_URI = 'http://localhost:3000/auth/callback';
+exports.AUTH_URI = 'https://github.com/login/oauth/authorize';
+exports.TOKEN_URI = 'https://github.com/login/oauth/access_token';
